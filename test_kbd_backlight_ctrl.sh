cac5a7be1ad8617946f891881de3b27ddc9d66a2
ruben2020 <ruben2020-git@users.sourceforge.net>
2019-12-26 20:23:07 +0800

    Updated to latest values that work on Arch Linux

--- test_kbd_backlight_ctrl.sh
+++ test_kbd_backlight_ctrl.sh
@@ -4,6 +4,6 @@
 ##############################################
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/var/run/dbus/system_bus_socket
 export KBD_BACKLIGHT_CTRL_TIMEOUT=3
-export KBD_BACKLIGHT_CTRL_INDEVICE=/dev/input/event0
+export KBD_BACKLIGHT_CTRL_INDEVICE=/dev/input/event4
 time ./kbd_backlight_ctrl
 
