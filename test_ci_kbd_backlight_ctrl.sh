2343b0e12126ef80bf133cfce35f368e1e669e45
ruben2020 <ruben2020-git@users.sourceforge.net>
2020-01-04 23:23:33 +0800

    Fixing Travis 3

--- test_ci_kbd_backlight_ctrl.sh
+++ test_ci_kbd_backlight_ctrl.sh
@@ -4,6 +4,6 @@
 ##############################################
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/var/run/dbus/system_bus_socket
 export KBD_BACKLIGHT_CTRL_TIMEOUT=10
-export KBD_BACKLIGHT_CTRL_INDEVICE=/dev/input/event0
+export KBD_BACKLIGHT_CTRL_INDEVICE=/dev/input/by-path/platform-i8042-serio-0-event-kbd
 timeout 20s kbd_backlight_ctrl
 
